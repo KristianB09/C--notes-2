fe807a204e99eb6506755fc32cf679f9f4d488e2
kristianb09 <kristianb09.kodehode@gmail.com>
2024-03-11 12:46:05 +0100

    Completed task 2 of module 4

--- Program.cs
+++ Program.cs
@@ -769,4 +769,116 @@ Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-2
 
 // Lesson 2
 
+/* int first = 2;
+string second = "4";
+string result = first + second;
 
+Console.WriteLine(result);
+ */
+
+/* int myInt = 3;
+Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}"); */
+
+/* decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}"); */
+
+/* decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}"); */
+
+/* int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message); */
+
+/* string first = "5";
+string second = "7";
+
+int sum = int.Parse(first) + int.Parse(second);
+
+Console.WriteLine(sum);
+ */
+
+/* string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);   */
+
+/* int value = (int)1.5m;
+Console.WriteLine(value);
+
+int value2 = Convert.ToInt32(1.5m);
+Console.WriteLine(value2); */
+
+/* string value = "bad";
+int result = 0;
+
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to parse the measure");
+}
+
+if (result > 0)
+    Console.WriteLine($"Measurement (w/ offset): {50 + result}"); */
+
+
+// EXERCISE 1 - combine string array values as strings and as integers
+
+/* string[] values = { "12,3", "45", "ABC", "11", "DEF" };
+
+decimal currentNumber = 0;
+
+decimal totalNumber = 0;
+
+string alphabetical = "";
+
+foreach (string value in values)
+{
+    if (decimal.TryParse(value, out currentNumber))
+    {
+        totalNumber += currentNumber;
+    }
+    else
+    {
+        alphabetical += value;
+    }
+}
+
+Console.WriteLine($"Message: {alphabetical}");
+Console.WriteLine($"Total: {totalNumber}");
+ */
+
+// EXERCISE 2 - output math operations as specific number types
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+decimal decimalResult1 = value1 / value2;
+
+int result1 = Convert.ToInt32(decimalResult1);
+
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal decimalValue3 = (decimal)value3;
+decimal result2 = value2 / decimalValue3;
+
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+
+
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
